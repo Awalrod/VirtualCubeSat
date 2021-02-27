327ec53ef51740b70f949f6f5be1c9b510b3cd8a
Alexander <alex@uahparkorcement.lan>
2021-02-27 15:01:18 -0600

    Syntax error in client

--- client/cubesat-client.py
+++ client/cubesat-client.py
@@ -13,7 +13,7 @@ def sendPayload(conn):
 	genericPayload2 = random.randint(0,100)
 	message = '{"BATT":'+str(battery)+', "DATA1":'+str(genericPayload1)+', "DATA2":'+str(genericPayload2)+'}'
 	print('Sending:'+message)
-	s.sendall(message)
+	conn.sendall(message)
 
 def main():
 	HOST = '192.168.100.4'
